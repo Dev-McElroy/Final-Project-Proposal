66901d1eb2a129a72511d23a9e67e7d097e2d00a
Dev-McElroy <dcm230002@utdallas.edu>
2024-12-03 16:20:28 -0600

    intial set up of comments and main function

---
+++ src/finalProject.py
@@ -0,0 +1,16 @@
+import pygame
+
+#Mode 1, Color saving
+    #Introduce ability to input color values (3 Hex Codes)
+        #ask if the user would like to save that value
+        #request a name for the color
+#Mode 2, Color Display
+    #ask the user what color they would like to display, this can be entered as a name, or hex code.
+    #Display the color to the monitor for a set amount of time
+    #Quit teh display and let the user request anotehr color, or let them quit
+
+def main():
+    print("hi")
+
+if __name__ == "__main__":
+    main()
