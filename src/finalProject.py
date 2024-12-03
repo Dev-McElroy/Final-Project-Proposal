9f3c8c9142db86ae3487bf3c147d1890c74b64eb
Dev-McElroy <dcm230002@utdallas.edu>
2024-12-03 16:33:29 -0600

    creating color library, and initial loop

--- src/finalProject.py
+++ src/finalProject.py
@@ -10,7 +10,14 @@ import pygame
     #Quit teh display and let the user request anotehr color, or let them quit
 
 def main():
-    print("hi")
+    print("Hello, welcome to the Dev McElroy Color picker!")
+    print("lets get started")
+    quit = False
+    while(quit == False):
+        R = input(f"please enter the 'R' value in the RGB code of your color: ")
+        G = input(f"please enter the 'G' value in the RGB code of your color: ")
+        B = input(f"please enter the 'B' value in the RGB code of your color: ")
+        colorName = input("Please enter the name of your color")
 
 if __name__ == "__main__":
     main()
