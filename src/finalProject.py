54eb6c743651739e9ba17fe75f5764bc9a5f06fa
Dev-McElroy <dcm230002@utdallas.edu>
2024-12-03 17:10:28 -0600

    utilizing a CSV file to store the information about each color

--- src/finalProject.py
+++ src/finalProject.py
@@ -1,4 +1,5 @@
 import pygame
+import csv
 
 #Mode 1, Color saving
     #Introduce ability to input color values (3 Hex Codes)
@@ -13,11 +14,19 @@ def main():
     print("Hello, welcome to the Dev McElroy Color picker!")
     print("lets get started")
     quit = False
+    i = 0
     while(quit == False):
-        R = input(f"please enter the 'R' value in the RGB code of your color: ")
-        G = input(f"please enter the 'G' value in the RGB code of your color: ")
-        B = input(f"please enter the 'B' value in the RGB code of your color: ")
-        colorName = input("Please enter the name of your color")
+        R = int(input(f"please enter the 'R' value in the RGB code of your color: "))
+        G = int(input(f"please enter the 'G' value in the RGB code of your color: "))
+        B = int(input(f"please enter the 'B' value in the RGB code of your color: "))
+        colorName = input("Please enter the name of your color: ")
+        with open("colorNames.csv", "a") as add_item:
+            csv_writer = csv.writer(add_item)
+            csv_writer.writerow([colorName, R, G, B])
+        if((input("If you have no other colors to enter type 'q' to quit, or enter anything else to continue entering colors")).lower() == 'q'):
+            quit = True
+
+
 
 if __name__ == "__main__":
     main()
